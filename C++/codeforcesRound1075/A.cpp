9efcc19f0d67cc2216729d9dd32b6122fbd55218
Johnny's MacBook Pro M3 <johnny.ganyu@outlook.com>
2026-01-23 23:44:11 +0800

    feat(A.cpp): 添加代码逻辑以处理输入并计算结果

--- C++/codeforcesRound1075/A.cpp
+++ C++/codeforcesRound1075/A.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <algorithm>
 using namespace std;
 
 int main(void) {
@@ -8,12 +9,29 @@ int main(void) {
     {
         int n, h, l;
         cin >> n >> h >> l;
-        
+        int a[n];
         for (int i = 0; i < n; i++)
         {
-            /* code */
+            cin >> a[i];
         }
-        
+        sort(a,a+n);
+        int cnt1 = 0, cnt2 = 0, sum = 0;
+        for (int i = 0; i < n; i++)
+        {
+            if (a[i] <= h) {
+                cnt1++;
+            }
+            if (a[i] <= l) {
+                cnt2++;
+            }
+            
+        }
+        if (cnt1>=2*cnt2||cnt2>=2*cnt1)
+        {
+            cout << min(cnt1,cnt2);
+        }
+        else cout << max(cnt1,cnt2)/2;
+        cout << endl;
     }
     
 }
