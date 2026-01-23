71ee93860fb4f3ffdb1b4e9a98dcab021a3b1f9c
Johnny's MacBook Pro M3 <johnny.ganyu@outlook.com>
2026-01-23 23:11:26 +0800

    Add various C language programs and examples
    - Introduced multiple C programs covering different topics including:
      - Area calculation of rectangles
      - Euclidean algorithm for GCD
      - Red packet distribution simulation
      - Average score calculation
      - Prime number generation
      - Function value calculation based on input
      - Simple variable swapping
      - Triangle printing using recursion
      - Chinese numeral representation of digit sums
      - Standard weight calculation based on height
      - Basic arithmetic operations
    
    - Added necessary executable files and source code for each program.
    - Included task configuration for building C programs in Visual Studio Code.

---
+++ C++/codeforcesRound1075/A.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main(void) {
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n, h, l;
+        cin >> n >> h >> l;
+        
+        for (int i = 0; i < n; i++)
+        {
+            /* code */
+        }
+        
+    }
+    
+}
